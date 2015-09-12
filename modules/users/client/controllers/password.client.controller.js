90234647f900ffaf0777e2c11aedfb5f0e744267
Lovell Felix <lovell2juicy@yahoo.com>
2015-09-11 20:04:55 -0500

    Revert "Fetched some updates from meanjs repositorys and resolved some merge conflicts"

--- modules/users/client/controllers/password.client.controller.js
+++ modules/users/client/controllers/password.client.controller.js
@@ -1,59 +1,44 @@
 'use strict';
 
-angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$http', '$location', 'Authentication', 'PasswordValidator',
-  function ($scope, $stateParams, $http, $location, Authentication, PasswordValidator) {
-    $scope.authentication = Authentication;
-    $scope.popoverMsg = PasswordValidator.getPopoverMsg();
+angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
+	function($scope, $stateParams, $http, $location, Authentication) {
+		$scope.authentication = Authentication;
 
-    //If user is signed in then redirect back home
-    if ($scope.authentication.user) {
-      $location.path('/');
-    }
+		//If user is signed in then redirect back home
+		if ($scope.authentication.user) $location.path('/');
 
-    // Submit forgotten password account id
-    $scope.askForPasswordReset = function (isValid) {
-      $scope.success = $scope.error = null;
+		// Submit forgotten password account id
+		$scope.askForPasswordReset = function() {
+			$scope.success = $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'forgotPasswordForm');
+			$http.post('/api/auth/forgot', $scope.credentials).success(function(response) {
+				// Show user success message and clear form
+				$scope.credentials = null;
+				$scope.success = response.message;
 
-        return false;
-      }
+			}).error(function(response) {
+				// Show user error message and clear form
+				$scope.credentials = null;
+				$scope.error = response.message;
+			});
+		};
 
-      $http.post('/api/auth/forgot', $scope.credentials).success(function (response) {
-        // Show user success message and clear form
-        $scope.credentials = null;
-        $scope.success = response.message;
+		// Change user password
+		$scope.resetUserPassword = function() {
+			$scope.success = $scope.error = null;
 
-      }).error(function (response) {
-        // Show user error message and clear form
-        $scope.credentials = null;
-        $scope.error = response.message;
-      });
-    };
+			$http.post('/api/auth/reset/' + $stateParams.token, $scope.passwordDetails).success(function(response) {
+				// If successful show success message and clear form
+				$scope.passwordDetails = null;
 
-    // Change user password
-    $scope.resetUserPassword = function (isValid) {
-      $scope.success = $scope.error = null;
+				// Attach user profile
+				Authentication.user = response;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'resetPasswordForm');
-
-        return false;
-      }
-
-      $http.post('/api/auth/reset/' + $stateParams.token, $scope.passwordDetails).success(function (response) {
-        // If successful show success message and clear form
-        $scope.passwordDetails = null;
-
-        // Attach user profile
-        Authentication.user = response;
-
-        // And redirect to the index page
-        $location.path('/password/reset/success');
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
-  }
+				// And redirect to the index page
+				$location.path('/password/reset/success');
+			}).error(function(response) {
+				$scope.error = response.message;
+			});
+		};
+	}
 ]);
--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -1,29 +1,24 @@
 'use strict';
 
 angular.module('users').controller('EditProfileController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-  function ($scope, $http, $location, Users, Authentication) {
-    $scope.user = Authentication.user;
+	function($scope, $http, $location, Users, Authentication) {
+		$scope.user = Authentication.user;
 
-    // Update a user profile
-    $scope.updateUserProfile = function (isValid) {
-      $scope.success = $scope.error = null;
+		// Update a user profile
+		$scope.updateUserProfile = function(isValid) {
+			if (isValid){
+				$scope.success = $scope.error = null;
+				var user = new Users($scope.user);
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
-
-        return false;
-      }
-
-      var user = new Users($scope.user);
-
-      user.$update(function (response) {
-        $scope.$broadcast('show-errors-reset', 'userForm');
-
-        $scope.success = true;
-        Authentication.user = response;
-      }, function (response) {
-        $scope.error = response.data.message;
-      });
-    };
-  }
+				user.$update(function(response) {
+					$scope.success = true;
+					Authentication.user = response;
+				}, function(response) {
+					$scope.error = response.data.message;
+				});
+			} else {
+				$scope.submitted = true;
+			}
+		};
+	}
 ]);
